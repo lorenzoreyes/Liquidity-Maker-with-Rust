80c168589e216f6696018833ebe616f7bf610138
Lorenzo Reyes <56459386+lorenzoreyes@users.noreply.github.com>
2022-07-25 01:41:43 -0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -27,8 +27,9 @@ fn main() {
     let mut ticket5 = String::new();
     io::stdin().read_line(&mut ticket5).expect("Failed to read pair");
     println!("{} was the last pair as input",ticket5);
-    // see-the-magic-of-simplicity
+    
     let binance_url = format!("{}/stream?streams={}@depth10@100ms/{}@depth10@100ms/{}@depth10@100ms/{}@depth10@100ms/{}@depth10@100ms", BINANCE_WS_API,ticket,ticket2,ticket3,ticket4,ticket5);
+    
     println!("You requested the following stream: {}", binance_url);
 
     let (mut socket, response) = connect(Url::parse(&binance_url).unwrap()).expect("Can't connect.");
@@ -57,33 +58,3 @@ fn main() {
         }
     }
 }
-/*
-        let parsed_data: serde_json::Value = serde_json::from_str(&msg).expect("Unable to parse message");
-        //println!("{:?}", parsed_data);
-        println!("best ask: {}, ask size: {}", parsed_data["asks"][0][0], parsed_data["asks"][0][1]);
-    }
-}
-*/
-
-/* Part to handle user input to obtain configurable pairs
-    println!("Provide 1rst pair for the stream\n");
-    let mut ticket = String::new();
-    io::stdin().read_line(&mut ticket).expect("Failed to read pair");
-    // 2nd
-    println!("First one was: {}.\n Now provide Second pair for the stream",ticket.to_uppercase());
-    let mut ticket2 = String::new();
-    io::stdin().read_line(&mut ticket2).expect("Failed to read pair");
-    // 3rd
-    println!("2nd one: {}.\nNow the 3rd pair for the stream",ticket2);
-    let mut ticket3 = String::new();
-    io::stdin().read_line(&mut ticket3).expect("Failed to read pair");
-    // 4th
-    println!("3rd: {}.\nProvide 4th pair for the stream",ticket3);
-    let mut ticket4 = String::new();
-    io::stdin().read_line(&mut ticket4).expect("Failed to read pair");
-    // 5th and last one
-    println!("4th one: {}.\nProvide 5th and last pair for the stream",ticket4);
-    let mut ticket5 = String::new();
-    io::stdin().read_line(&mut ticket5).expect("Failed to read pair");
-    println!("{} was the last pair as input",ticket5);
-*/
